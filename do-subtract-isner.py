27cef36e32b548a76385ac460e40463a3d16a2e9
Kirk Amundson <kirkamundson10@gmail.com>
2017-03-10 22:57:22 -0800

    do-subtract-isner.py now tracks k-mer enrichment in each fastq

--- do-subtract-isner.py
+++ do-subtract-isner.py
@@ -19,13 +19,13 @@ def main():
     p.add_argument('A')
     p.add_argument('B')
     #p.add_argument('C')
-    #p.add_argument('output')
+    p.add_argument('output')
     p.add_argument('-k', '--ksize', default=31, type=int)
     args = p.parse_args()
 
     # for potato, would suggest changing 1e6 to 4e9. Only matters for A-B hash.
-    a_table = khmer.Nodetable(args.ksize, 1e6, 4)
-    b_table = khmer.Nodetable(args.ksize, 1e6, 4)
+    a_table = khmer.Nodetable(args.ksize, 10e9, 4)
+    b_table = khmer.Nodetable(args.ksize, 10e9, 4)
     b_table.consume_seqfile(args.B)
 
     n_hashes_loaded = 0
@@ -38,20 +38,38 @@ def main():
     print('loaded {} k-mers in A but not in B'.format(n_hashes_loaded))
     # at this point, a_table contains A - B
     
+    out_table = open(args.output, 'w')
+    head = 'sample\ttotal-read\thap-kmer-read\ttotal-kmer\tkmer-hits\n'
+    out_line = '' # one string to rule them all
+    
     for fastq in fastqGrab():
-        print('Now querying',fastq)
+        print('Now querying {}'.format(fastq))
+        hitcount = 0 # initialize hitcount
+        hashcount = 0 # initialize hashcount
         out_fp = open(fastq+'.subset', 'w')
         n_read = 0
         n_written = 0
+        
         for record in khmer.utils.clean_input_reads(screed.open(fastq)):
             n_read += 1
+            match = 0 # adopt O state for no hits in read, change to 1 when match in read
+            
             for hash in a_table.get_kmer_hashes(record.cleaned_seq):
+                hashcount += 1
                 if a_table.get(hash):
-                    khmer.utils.write_record(record, out_fp) # Probably very slow I/O constraints.
-                    n_written += 1
-                    break
+                    match = 1 # swtich to state "match"
+                    hitcount += 1 # tally k-mer hit
+                    
+            if match == 1: # write to file if switch tripped
+                khmer.utils.write_record(record, out_fp)
+                n_written += 1
+                #break
+                
         print('wrote {} of {} records for {}'.format(n_written, n_read,
                                                 fastq))
+        out_line += fastq + '\t'+ str(n_read) + '\t' + str(n_written) + '\t' + str(hashcount) + '\t' + str(hitcount) + '\n'
+        
+    out_table.write(head+out_line)
 
 if __name__ == '__main__':
     main()
